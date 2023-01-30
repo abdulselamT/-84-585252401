229c80181750852c85ff417af4fcda029d2e01ee
aosqa <75848455+abdulselamT@users.noreply.github.com>
2023-01-31 01:33:37 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,26 @@
+api_id_bt=29389397
+api_hash_bt = '4bf3d4ca50a9ed8660cae0376f32e8dc'
+phone_bt = '+84585252401'
+bt_username="@aosqa22"
+manager_bt="@aosqa2"
+plain_text="""We are a team of media buyers, we have high quality of traffic. We are looking for new clients for the FX/Crypto niches.
+We guarantee:
+- low prices
+- high volume
+- high Conversion rate
+- 24/7 support
+-----
+Мы команда медиабайеров, у нас высокое качество трафика. Мы ищем новых клиентов для ниш FX/Crypto.
+Мы гарантируем:
+- Низкие цены
+- большой объем
+- высокая конверсия
+- Круглосуточная поддержка 7 дней в неделю
+-----
+אנחנו צוות של מדיה ביירים, יש לנו איכות גבוהה של טראפיק. אנו מחפשים לקוחות חדשים של-FX/Crypto.
+אנחנו מבטיחים:
+- מחירים נמוכים
+- ווליום גבוה
+- שיעור המרה גבוה
+- תמיכה 24/7
+"""
